2d6b63de8f3259f5f56f67b9b1a76a2a9a9f911b
tomayac <steiner.thomas@gmail.com>
2016-05-19 13:00:32 +0200

    Reduced to a barebone static template.

--- slide_config.js
+++ slide_config.js
@@ -12,7 +12,7 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/google_developers.svg',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -37,4 +37,3 @@ var SLIDE_CONFIG = {
     github: 'http://github.com/you'
   }*/]
 };
-
